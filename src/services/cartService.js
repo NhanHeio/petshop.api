2cd14a7727f9487b0803aa3b7658cdba5f735927
NhanHeio <hotrungnhan2000@gmail.com>
2022-03-24 17:53:15 +0700

    Add table product sold

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,23 +1,23 @@
 import db from "../models/index";
-import {sendEmail} from './emailService';
+import { sendEmail } from './emailService';
 let getCart = (user_id) => {
-    return new Promise (async (resolve, reject) => {
-        try{
+    return new Promise(async (resolve, reject) => {
+        try {
             let cart = ''
-            
+
             cart = await db.Cart.findOne({
-                where: {user_id: user_id}
+                where: { user_id: user_id }
             })
-            if(!cart){
+            if (!cart) {
                 resolve(cart = "")
             }
             let cart_items = ''
             cart_items = await db.CartItem.findAll({
-                where: { cart_id: cart.id}
+                where: { cart_id: cart.id }
             })
             cart.cartItems = cart_items
             resolve(cart)
-        }catch(e){
+        } catch (e) {
             reject(e);
         }
     })
@@ -25,7 +25,7 @@ let getCart = (user_id) => {
 
 let deleteCartProduct = (id) => {
     return new Promise(async (resolve, reject) => {
-        try{
+        try {
             // let cartItem = await db.CartItem.findOne({
             //     where: {id: id}
             // })
@@ -38,52 +38,52 @@ let deleteCartProduct = (id) => {
             let quantity = product.quantity
             let product_id = product.product_id
             let cart = await db.Cart.findOne({
-                where: {id: cart_id},
+                where: { id: cart_id },
             })
-            if(cart){
+            if (cart) {
                 await db.Cart.update({
-                    quantity : cart.quantity - quantity,
-                    total_price : cart.total_price - price,
+                    quantity: cart.quantity - quantity,
+                    total_price: cart.total_price - price,
                     updatedAt: new Date()
-                },{
-                    where: {id: cart_id},
+                }, {
+                    where: { id: cart_id },
                 })
                 console.log(cart)
                 await db.CartItem.destroy({
-                    where: {id: id}
+                    where: { id: id }
                 })
             }
             let product_detail = await db.Product.findOne({
-                where: {id: product_id}
+                where: { id: product_id }
             })
             await db.Product.update({
                 quantity: product_detail.quantity + quantity
-            },{
-                where: {id: product_id}
+            }, {
+                where: { id: product_id }
             })
             cart = await db.Cart.findOne({
-                where: {id: cart_id},
+                where: { id: cart_id },
             })
             let cart_items = ''
             cart_items = await db.CartItem.findAll({
-                where: { cart_id: cart.id}
+                where: { cart_id: cart.id }
             })
             cart.cartItems = cart_items
             resolve(cart)
-        }catch(e){
+        } catch (e) {
             reject(e);
         }
     })
 }
 
-let addToCart = (user_id,product_id,quantity) => {
+let addToCart = (user_id, product_id, quantity) => {
     return new Promise(async (resolve, reject) => {
         try {
             let cart = ''
             cart = await db.Cart.findOne({
-                where: {user_id: user_id}
+                where: { user_id: user_id }
             })
-            if(!cart){
+            if (!cart) {
                 await db.Cart.create({
                     user_id: user_id,
                     quantity: 0,
@@ -91,36 +91,40 @@ let addToCart = (user_id,product_id,quantity) => {
                     createdAt: new Date(),
                     updatedAt: new Date()
                 })
-                 cart = await db.Cart.findOne({
-                    where: {user_id: user_id}
+                cart = await db.Cart.findOne({
+                    where: { user_id: user_id }
                 })
             }
             let product = await db.Product.findOne({
-                where: {id: product_id},
+                where: { id: product_id },
             })
-            if(product.quantity > quantity) {
+            if (product.quantity > quantity) {
                 await db.Product.update({
                     quantity: product.quantity - quantity,
                     updatedAt: new Date()
-                },{
-                    where: {id: product_id},
+                }, {
+                    where: { id: product_id },
                 })
-                
+
                 let cart_id = cart.id
                 let product_name = product.name
                 let product_price = product.price
                 let product_img = product.img
                 let cart_item = await db.CartItem.findOne({
-                    where: {product_id: product_id}
+                    where: {
+                        product_id: product_id,
+                        cart_id: cart_id
+                    }
                 })
-                if(cart_item){
+                console.log(cart_item)
+                if (cart_item) {
                     await db.CartItem.update({
                         quantity: cart_item.quantity + quantity,
                         updatedAt: new Date()
-                    },{
-                        where: {product_id: product_id}
+                    }, {
+                        where: { product_id: product_id }
                     })
-                }else{
+                } else {
                     await db.CartItem.create({
                         cart_id: cart_id,
                         product_id: product_id,
@@ -134,38 +138,32 @@ let addToCart = (user_id,product_id,quantity) => {
                     quantity: cart.quantity + quantity,
                     total_price: cart.total_price + product_price * quantity,
                     updatedAt: new Date()
-                },{
-                    where: {id: cart_id}
-                })       
-                
-            }else{
+                }, {
+                    where: { id: cart_id }
+                })
+
+            } else {
                 resolve({
                     errCode: 1,
                     errMessage: 'Product no available'
                 })
             }
             resolve(cart)
-        }catch(e){
+        } catch (e) {
             reject(e);
         }
     })
 }
 
-let checkoutOrder = (user_id,name,phoneNumber,address) => {
+let checkoutOrder = (user_id, name, phoneNumber, address) => {
     return new Promise(async (resolve, reject) => {
         try {
-            
-
             let cart = await db.Cart.findOne({
-                where: { user_id: user_id}
+                where: { user_id: user_id }
             })
-
-            console.log(cart)
             let cartItems = await db.CartItem.findAll({
-                where: { cart_id: cart.id},
+                where: { cart_id: cart.id },
             })
-            console.log(cartItems)
-
             await db.Order.create({
                 user_id: user_id,
                 username: name,
@@ -177,14 +175,14 @@ let checkoutOrder = (user_id,name,phoneNumber,address) => {
                 updatedAt: new Date()
             })
             let order = await db.Order.findOne({
-                where: {user_id: user_id},
+                where: { user_id: user_id },
                 order: [
-                    ['id','DESC']
+                    ['id', 'DESC']
                 ]
             })
 
-            await cartItems.forEach(item => {
-                    db.OrderItem.create({
+            await cartItems.forEach(async(item) => {
+                await db.OrderItem.create({
                     order_id: order.id,
                     product_id: item.product_id,
                     product_name: item.product_name,
@@ -194,31 +192,54 @@ let checkoutOrder = (user_id,name,phoneNumber,address) => {
                     createdAt: new Date(),
                     updatedAt: new Date()
                 })
-                db.CartItem.destroy({
-                    where: {id: item.id},
+                let product = await db.Product.findOne({
+                    where: { id: item.product_id}
+                })
+                let productSold = await db.Sold.findOne({
+                    where: { id: item.product_id}
+                })
+                if(productSold){
+                    await db.Sold.update({
+                        quantity : productSold.quantity + item.quantity,
+                        updatedAt: new Date()
+                    },{
+                        where: {product_id: productSold.product_id}
+                    })
+                }else{
+                    await db.Sold.create({
+                        product_id: product.id,
+                        name: product.name,
+                        type_id: product.type_id,
+                        quantity: item.quantity,
+                        createdAt: new Date(),
+                        updatedAt: new Date()
+                    })
+                }
+                await db.CartItem.destroy({
+                    where: { id: item.id },
                 })
             });
             await db.Cart.destroy({
-                where: {user_id: user_id}
+                where: { user_id: user_id }
             })
             resolve(order)
-        }catch(e){
+        } catch (e) {
             reject(e);
         }
     })
 }
 
 let getOrderUser = (userID) => {
-    return new Promise (async (resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         try {
             let order = {}
             let orderList = ''
             let user = await db.User.findOne({
-                where: {id: userID}
+                where: { id: userID }
             })
-            if(user){
+            if (user) {
                 orderList = await db.Order.findAll({
-                    where: {user_id: userID}
+                    where: { user_id: userID }
                 })
                 resolve(order = {
                     errCode: 0,
@@ -230,7 +251,7 @@ let getOrderUser = (userID) => {
                 errCode: 1,
                 errMessage: 'User not found!'
             })
-        }catch (e) {
+        } catch (e) {
             reject(e);
         }
     })
@@ -241,35 +262,35 @@ let getOrder = (id) => {
         try {
             let orderItem = {}
             let order = await db.OrderItem.findAll({
-                where: {order_id: id}
+                where: { order_id: id }
             })
             resolve(orderItem = {
                 errCode: 0,
                 errMessage: 'Fetch Order Item successfully!',
                 order
             })
-        }catch (e) {
+        } catch (e) {
             reject(e);
         }
     })
 }
 
-let cancelOrder = (id,userID) => {
+let cancelOrder = (id, userID) => {
     return new Promise(async (resolve, reject) => {
-        try{
+        try {
             let order = {}
             let orderList = ''
             let user = await db.User.findOne({
-                where: {id: userID}
+                where: { id: userID }
             })
-            if(user){
+            if (user) {
                 await db.Order.update({
                     status: 'Cancel'
-                },{
-                    where: {id: id}
+                }, {
+                    where: { id: id }
                 })
                 orderList = await db.Order.findAll({
-                    where: {user_id: userID}
+                    where: { user_id: userID }
                 })
                 resolve(order = {
                     errCode: 0,
@@ -281,7 +302,7 @@ let cancelOrder = (id,userID) => {
                 errCode: 1,
                 errMessage: 'User not found!'
             })
-        }catch (e) {
+        } catch (e) {
             reject(e);
         }
     })
