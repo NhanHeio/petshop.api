94529306cc882bcd5b9d97ecc6a1d0958f1d707c
NhanHeio <hotrungnhan2000@gmail.com>
2022-04-04 02:39:27 +0700

    deploy 7

--- src/server.js
+++ src/server.js
@@ -13,8 +13,8 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     // res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Origin', 'https://petshop-react.herokuapp.com');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
